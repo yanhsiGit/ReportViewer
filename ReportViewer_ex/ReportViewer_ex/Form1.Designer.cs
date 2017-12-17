65886ea183f965d24ea29732147e9ac2a7ff6e63
Alex Lee <leyanhsi@gmail.com>
2017-12-17 16:21:55 +0800

    Add files via upload

---
+++ ReportViewer_ex/ReportViewer_ex/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ReportViewer_ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataSet1 = new ReportViewer_ex.DataSet1();
+            this.CompanyInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.CompanyInfoTableAdapter = new ReportViewer_ex.DataSet1TableAdapters.CompanyInfoTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CompanyInfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.CompanyInfoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReportViewer_ex.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(447, 314);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "DataSet1";
+            this.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // CompanyInfoBindingSource
+            // 
+            this.CompanyInfoBindingSource.DataMember = "CompanyInfo";
+            this.CompanyInfoBindingSource.DataSource = this.DataSet1;
+            // 
+            // CompanyInfoTableAdapter
+            // 
+            this.CompanyInfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 314);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "ReportViewer控制項範例程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CompanyInfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource CompanyInfoBindingSource;
+        private DataSet1 DataSet1;
+        private DataSet1TableAdapters.CompanyInfoTableAdapter CompanyInfoTableAdapter;
+    }
+}
+
---
+++ ReportViewer_ex/ReportViewer_ex/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportViewer_ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO:  這行程式碼會將資料載入 'DataSet1.CompanyInfo' 資料表。您可以視需要進行移動或移除。
+            //this.CompanyInfoTableAdapter.Fill(this.DataSet1.CompanyInfo);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
